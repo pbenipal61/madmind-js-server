036a6986912319fb89ac9ac7d61b6bcc3b39c6d6
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-03-26 12:16:02 +0200

    Bug fixes in port

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('MadMindQuiz', 'root12', 'pSeHrHjM1002',
     {
-        dialect: 'mysql', host: 'localhost', port: 8889,
+        dialect: 'mysql', host: 'localhost',
         logging: false
     });
 
