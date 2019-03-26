7fc151678b319816b9327902f5230601213f95b9
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-03-26 14:23:50 +0200

    HTTPS server

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 const app = require('./app');
-
+const https = require("https");
+const fs = require("fs");
 const port = 3000;
+const https_port = 443;
 
 
 
@@ -10,6 +12,21 @@ const http_server = http.createServer(app);
 
 http_server.listen(port, () => {
     console.log("HTTP server up");
+});
+
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/joelmaenpaa.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/joelmaenpaa.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/joelmaenpaa.com/chain.pem', 'utf8');
+
+const credentials = {
+    key: privateKey,
+    cert: certificate,
+    ca: ca
+};
+
+const https_server = https.createServer(credentials, app);
+https_server.listen(https_port, () => {
+    console.log(`HTTPS server up`);
 });
 
 
