e1c8909ed705a47bdb0d256a432833b1e1f6e86e
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-03-26 13:06:00 +0200

    Bug fixes in questions.ejs and main.js

--- controllers/main.js
+++ controllers/main.js
@@ -35,147 +35,161 @@ exports.postAddQuestion = (req, res, next) => {
     var categoryTitle = req.body.categoryTitle;
     var newCategoryTitle = req.body.newCategory;
 
-    var categoryStatus = req.body.categoryStatus;
-    var question = req.body.question;
-    var correctAnswer = req.body.correctAnswer;
+    Category.findOne({ where: { title: newCategoryTitle } }).then(category => {
 
-    var options = [];
-    var option1 = req.body.option1;
-    if (option1 != null && option1 != "") {
-        options.push(option1);
-    }
-    var option2 = req.body.option2;
-    if (option2 != null && option2 != "") {
-        options.push(option2);
-    }
-    var option3 = req.body.option3;
-    if (option3 != null && option3 != "") {
-        options.push(option3);
-    }
+        if (category != null) {
 
+            categoryId = category["id"];
+        }
 
-    var usePermission = 0;
-    console.log(
-        "options length is ", options.length
-    );
-    if ((options.length >= 3) && (question != null && question != "") && (correctAnswer != null && correctAnswer != "")) {
-        console.log("changing use permission");
-        usePermission = 1;
-    }
+        var categoryStatus = req.body.categoryStatus;
+        var question = req.body.question;
+        var correctAnswer = req.body.correctAnswer;
+
+        var options = [];
+        var option1 = req.body.option1;
+        if (option1 != null && option1 != "") {
+            options.push(option1);
+        }
+        var option2 = req.body.option2;
+        if (option2 != null && option2 != "") {
+            options.push(option2);
+        }
+        var option3 = req.body.option3;
+        if (option3 != null && option3 != "") {
+            options.push(option3);
+        }
 
 
-    console.log(categoryId, question, correctAnswer, categoryTitle);
-
-    if (question != null && correctAnswer != null && option1 != null && option2 != null && option3 != null) {
-        if (categoryId != -1 && categoryId != -2) {
-
-
-            Question.create({
-                question: question,
-                categoryId: categoryId,
-                categoryTitle: categoryTitle,
-                correctAnswer: correctAnswer,
-                option1: option1,
-                option2: option2,
-                option3: option3,
-
-                option1Guesses: 0,
-                option2Guesses: 0,
-                option3Guesses: 0,
+        var usePermission = 0;
+        console.log(
+            "options length is ", options.length
+        );
+        if ((options.length >= 3) && (question != null && question != "") && (correctAnswer != null && correctAnswer != "")) {
+            console.log("changing use permission");
+            usePermission = 1;
+        }
 
 
-                usePermission: usePermission
+        console.log(categoryId, question, correctAnswer, categoryTitle);
 
-            }).then(result => {
-                console.log("Question added");
+        if (question != null && correctAnswer != null && option1 != null && option2 != null && option3 != null) {
+            if (categoryId != -1 && categoryId != -2) {
 
 
-                res.status(201).send({
-                    message: "Question added successfully",
-                    status: 201,
-                    result: result
-                });
-            })
-                .catch(err => {
-                    console.log("Error in adding question. " + err);
-                    res.status(400).send({
-                        message: "Failed to add question",
-                        error: err,
-                        status: 400
+                Question.create({
+                    question: question,
+                    categoryId: categoryId,
+                    categoryTitle: categoryTitle,
+                    correctAnswer: correctAnswer,
+                    option1: option1,
+                    option2: option2,
+                    option3: option3,
+
+                    option1Guesses: 0,
+                    option2Guesses: 0,
+                    option3Guesses: 0,
+
+
+                    usePermission: usePermission
+
+                }).then(result => {
+                    console.log("Question added");
+
+
+                    res.status(201).send({
+                        message: "Question added successfully",
+                        status: 201,
+                        result: result
                     });
-                });
-
-
-
-
-        } else {
-
-
-            if (categoryId == -1) {
-
-                console.log("Failed to realise a category for the question");
-                res.status(400).send({
-                    message: "Failed to realise a category for the question",
-                    status: 400
-                });
-
-            } else {
-
-                Category.create({
-                    title: newCategoryTitle,
-                    status: categoryStatus,
-
                 })
-                    .then(result => {
-                        categoryId = result["dataValues"].id;
-                        Question.create({
-                            question: question,
-                            categoryId: categoryId,
-                            categoryTitle: categoryTitle,
-                            correctAnswer: correctAnswer,
-                            option1: option1,
-                            option2: option2,
-                            option3: option3,
-
-                            option1Guesses: 0,
-                            option2Guesses: 0,
-                            option3Guesses: 0,
-
-
-                            usePermission: usePermission
-
-                        }).then(result2 => {
-                            console.log("Question added to the new category successfully");
-                            res.status(201).send({
-                                message: "Question added to the new category successfully",
-                                status: 201,
-                                result: result2
-                            });
-                        }).catch(err => {
-                            console.log("Error in adding question. " + err);
-                            res.status(400).send({
-                                message: "Failed to add question to the new category",
-                                error: err,
-                                status: 400
-                            });
-                        });
-                    })
                     .catch(err => {
-                        console.log("Error in adding new category. " + err);
-
+                        console.log("Error in adding question. " + err);
                         res.status(400).send({
-                            message: "Failed to add new category",
+                            message: "Failed to add question",
                             error: err,
                             status: 400
                         });
                     });
 
+
+
+
+            } else {
+
+
+                if (categoryId == -1) {
+
+                    console.log("Failed to realise a category for the question");
+                    res.status(400).send({
+                        message: "Failed to realise a category for the question",
+                        status: 400
+                    });
+
+                } else {
+
+                    Category.create({
+                        title: newCategoryTitle,
+                        status: categoryStatus,
+
+                    })
+                        .then(result => {
+                            categoryId = result["dataValues"].id;
+                            Question.create({
+                                question: question,
+                                categoryId: categoryId,
+                                categoryTitle: result["dataValues"].title,
+                                correctAnswer: correctAnswer,
+                                option1: option1,
+                                option2: option2,
+                                option3: option3,
+
+                                option1Guesses: 0,
+                                option2Guesses: 0,
+                                option3Guesses: 0,
+
+
+                                usePermission: usePermission
+
+                            }).then(result2 => {
+                                console.log("Question added to the new category successfully");
+                                res.status(201).send({
+                                    message: "Question added to the new category successfully",
+                                    status: 201,
+                                    result: result2
+                                });
+                            }).catch(err => {
+                                console.log("Error in adding question. " + err);
+                                res.status(400).send({
+                                    message: "Failed to add question to the new category",
+                                    error: err,
+                                    status: 400
+                                });
+                            });
+                        })
+                        .catch(err => {
+                            console.log("Error in adding new category. " + err);
+
+                            res.status(400).send({
+                                message: "Failed to add new category",
+                                error: err,
+                                status: 400
+                            });
+                        });
+
+                }
+
+
+
             }
-
-
-
         }
-    }
+
+
+
+
+
+    });
+
 
 
 
