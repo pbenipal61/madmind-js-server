810945d58d8afa68a4902a410b901c76b5245de4
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-03-27 10:49:40 +0200

    added update question route

--- controllers/main.js
+++ controllers/main.js
@@ -134,3 +134,80 @@ exports.categories = (req, res, next) => {
         .catch(err => { console.log("Error in fetching all categories list. " + err) });
 
 }
+
+exports.updateQuestion = async (req, res, next) => {
+
+    try {
+        var question = await Question.findByPk(req.params.id);
+
+        res.render('questions/updateQuestion', {
+
+            question: question,
+            path: '/'
+
+
+        });
+    } catch (e) {
+
+        res.send({
+            message: "error in finding a question with provided id"
+        })
+    }
+
+
+}
+
+exports.postUpdateQuestion = (req, res, next) => {
+
+    console.log("Updating the question ", req.params.id);
+
+    var question = req.body.question;
+    if (question == "undefined") {
+        question = "";
+    }
+    var correctAnswer = req.body.correctAnswer;
+    if (correctAnswer == "undefined") {
+        correctAnswer = "";
+    }
+    var option1 = req.body.option1;
+    if (option1 == "undefined") {
+        option1 = "";
+    }
+    var option2 = req.body.option2;
+    if (option2 == "undefined") {
+        option2 = "";
+    }
+    var option3 = req.body.option3;
+    if (option3 == "undefined") {
+        option3 = "";
+    }
+
+    Question.update({
+
+        question: question,
+        correctAnswer: correctAnswer,
+        option1: option1,
+        option2: option2,
+        option3: option3,
+
+
+
+
+    }, { returning: true, where: { id: req.params.id } })
+        .then(result => {
+            console.log("Question updated");
+            res.status(201).send({
+                message: "Question updated successfully",
+                status: 201
+            });
+        })
+        .catch(err => {
+            console.log("Error in updating question. " + err);
+            res.status(400).send({
+                message: "Failed to update question",
+                error: err,
+                status: 400
+            });
+        })
+
+}
--- routes/api.js
+++ routes/api.js
@@ -8,8 +8,8 @@ router.get('/addQuestion', main.addQuestion);
 router.post('/addQuestion', main.postAddQuestion);
 router.get('/questions', main.questions);
 router.get('/categories', main.categories);
-
-
+router.get('/updateQuestion/:id', main.updateQuestion);
+router.post('/updateQuestion/:id', main.postUpdateQuestion);
 
 
 module.exports = router;
